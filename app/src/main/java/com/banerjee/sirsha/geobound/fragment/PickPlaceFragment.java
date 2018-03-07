c97f56554142c240c9803dd9c051a328fd2c29c4
sirsha.banerjee <sirsha.banerjee@indianic.com>
2018-03-07 19:11:17 +0530

    Adding markers on click

--- app/src/main/java/com/banerjee/sirsha/geobound/fragment/PickPlaceFragment.java
+++ app/src/main/java/com/banerjee/sirsha/geobound/fragment/PickPlaceFragment.java
@@ -1,13 +1,12 @@
 package com.banerjee.sirsha.geobound.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
 
 import com.banerjee.sirsha.geobound.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -16,6 +15,9 @@ import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
 
 /**
  * Created by S.B. on 1/3/18.
@@ -23,15 +25,18 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class PickPlaceFragment extends Fragment implements OnMapReadyCallback {
 
-    MapView mMapView;
-    GoogleMap mGoogleMap;
+    private MapView mMapView;
+    private GoogleMap mGoogleMap;
+
+    private ArrayList<LatLng> arrLatlnglist;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_pick_place, container, false);
 
-        mMapView = (MapView) v.findViewById(R.id.mapview);
+        mMapView = v.findViewById(R.id.mapview);
         mMapView.onCreate(savedInstanceState);
         mMapView.getMapAsync(this); //this is important
 
@@ -39,15 +44,60 @@ public class PickPlaceFragment extends Fragment implements OnMapReadyCallback {
     }
 
     @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        arrLatlnglist = new ArrayList<>();
+    }
+
+    @Override
     public void onMapReady(GoogleMap googleMap) {
         mGoogleMap = googleMap;
-        final LatLng latLng = new LatLng(21,57);
+        final LatLng latLng = new LatLng(21, 57);
         mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
-        mGoogleMap.addMarker(new MarkerOptions().position(latLng));
+//        mGoogleMap.addMarker(new MarkerOptions().position(latLng));
 //        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
         //Move the camera to the user's location and zoom in!
         mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12.0f));
         mGoogleMap.getUiSettings().setScrollGesturesEnabled(true);
+
+        mGoogleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+            @Override
+            public void onMapClick(LatLng point) {
+                // TODO Auto-generated method stub
+                arrLatlnglist.add(point);
+//                mGoogleMap.clear();
+//                    mGoogleMap.addMarker(new MarkerOptions().position(point));
+                if (arrLatlnglist != null && arrLatlnglist.size() > 0) {
+
+                    addMarker(point);
+                }
+            }
+        });
+
+
+    }
+
+    private void addMarker(LatLng latLng) {
+        for (int i = 0; i < arrLatlnglist.size(); i++) {
+            if (arrLatlnglist.get(i) != latLng) {
+                mGoogleMap.addMarker(new MarkerOptions().position(latLng));
+            }
+        }
+
+        PolylineOptions line=
+                new PolylineOptions().add(new LatLng(21,
+                                57),
+                        new LatLng(22,
+                                57),
+                        new LatLng(23,
+                                57),
+                        new LatLng(21,
+                                57))
+                        .width(5).color(Color.RED);
+
+        mGoogleMap.addPolyline(line);
     }
 
     @Override
