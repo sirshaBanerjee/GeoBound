da7ed38325192c223be0817a8cd2f5338babc4a3
sirsha.banerjee <sirsha.banerjee@indianic.com>
2018-03-07 11:29:38 +0530

    1. Map loading 2. Setting marker 3. Base structure

--- app/src/main/java/com/banerjee/sirsha/geobound/fragment/PickPlaceFragment.java
+++ app/src/main/java/com/banerjee/sirsha/geobound/fragment/PickPlaceFragment.java
@@ -1,22 +1,19 @@
 package com.banerjee.sirsha.geobound.fragment;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import com.banerjee.sirsha.geobound.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -24,89 +21,62 @@ import com.google.android.gms.maps.model.MarkerOptions;
  * Created by S.B. on 1/3/18.
  */
 
-public class PickPlaceFragment extends Fragment {
+public class PickPlaceFragment extends Fragment implements OnMapReadyCallback {
 
     MapView mMapView;
-    private GoogleMap googleMap;
+    GoogleMap mGoogleMap;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_pick_place, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_pick_place, container, false);
 
-        mMapView = (MapView) rootView.findViewById(R.id.fragment_pick_place_mapView);
+        mMapView = (MapView) v.findViewById(R.id.mapview);
         mMapView.onCreate(savedInstanceState);
+        mMapView.getMapAsync(this); //this is important
 
-
-        mMapView.onResume(); // needed to get the map to display immediately
-
-        try {
-            MapsInitializer.initialize(getActivity().getApplicationContext());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        mMapView.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(GoogleMap mMap) {
-                googleMap = mMap;
-
-                // For showing a move to my location button
-                if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
-                }
-                googleMap.setMyLocationEnabled(true);
-
-                // For dropping a marker at a point on the Map
-                LatLng sydney = new LatLng(-34, 151);
-                googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
-
-                // For zooming automatically to the location of the marker
-                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
-                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-            }
-        });
-
-        return rootView;
+        return v;
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+        final LatLng latLng = new LatLng(21,57);
+        mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
+        mGoogleMap.addMarker(new MarkerOptions().position(latLng));
+//        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+        //Move the camera to the user's location and zoom in!
+        mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12.0f));
+        mGoogleMap.getUiSettings().setScrollGesturesEnabled(true);
+    }
 
     @Override
     public void onResume() {
         super.onResume();
-        if (mMapView != null) {
-            mMapView.onResume();
-        }
+        mMapView.onResume();
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        if (mMapView != null) {
-            mMapView.onPause();
-        }
+        mMapView.onPause();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if (mMapView != null) {
-            mMapView.onDestroy();
-        }
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mMapView.onSaveInstanceState(outState);
     }
 
     @Override
     public void onLowMemory() {
         super.onLowMemory();
-        if (mMapView != null) {
-            mMapView.onLowMemory();
-        }
+        mMapView.onLowMemory();
     }
-
 }
